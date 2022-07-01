83738c0f63beffd3d7bd68084248306a83d2e11f
Eliot <eliothead@live.com>
2022-07-01 15:42:16 -0400

    updates dom dynamic

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,9 @@ var userFormEl = document.querySelector("#user-form");
 
 var nameInputEl = document.querySelector("#username");
 
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
 var formSubmitHandler = function(event) {
   event.preventDefault();
   // get value from input element
@@ -25,10 +28,38 @@ var getUserRepos = function(user) {
   fetch(apiUrl).then(function(response) {
     console.log(response);
     response.json().then(function(data) {
-      console.log(data);
+      displayRepos(data, user)
     });
   });
 };
+
+var displayRepos = function(repos, searchTerm) {
+// clear old content
+repoContainerEl.textContent = "";
+repoSearchTerm.textContent= searchTerm;
+
+// loop over repos
+
+for (var i =0; i<repos.length; i++){
+  //format repo name
+  var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+  // create a container for each repo
+
+  var repoEl = document.createElement("div");
+  repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+  // create a span element to hold repository name
+  var titleEl = document.createElement("span");
+  titleEl.textContent = repoName
+
+  // append to container
+
+  repoEl.appendChild(titleEl);
+
+  repoContainerEl.appendChild(repoEl);
+}
+}
 
 userFormEl.addEventListener("submit", formSubmitHandler);
 
