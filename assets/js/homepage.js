775da651fd99b53241761ea5cf11a360cb517a01
Eliot <eliothead@live.com>
2022-07-01 14:57:57 -0400

    form input and event handler

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,22 @@
+var userFormEl = document.querySelector("#user-form");
+
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  // get value from input element
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  } else {
+    alert ("Please enter a GitHub username");
+  }
+
+};
+
+
 var getUserRepos = function(user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -11,4 +30,6 @@ var getUserRepos = function(user) {
   });
 };
 
-getUserRepos("lernantino");
+userFormEl.addEventListener("submit", formSubmitHandler);
+
+
