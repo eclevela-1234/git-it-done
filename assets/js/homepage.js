95fdcc0a9ad47d164524e7e917476bbc6d68d812
Eliot Cleveland <eclevela@richmond.edu>
2022-07-02 23:04:07 -0400

    featured language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,24 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+// languageButtonsEl.setAttribute("id", "language-buttons");
+
+var getFeaturedRepos = function(language){
+
+
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Something went wrong")
+    }
+  });
+};
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -90,5 +108,17 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
+var buttonClickHandler = function(event){
+  var language = event.target.getAttribute("data-language");
+
+  if (language) {
+   getFeaturedRepos(language);
+    repoContainerEl.textContent = "";
+  }
+
+}
+
+
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
